85e18742edeb61f17abf1b4d6e928ea6473d82d2
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-05-30 20:47:27 +1000

    Replace arc with zen, keep arc at 8 for now

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,12 +4,13 @@ MODIFIERS = { "cmd" } -- Modifiers used for app shortcuts
 -- App configuration
 APPS = {
 	{ shortcut = "1", name = "Ghostty" },
-	{ shortcut = "2", name = "Arc" },
+	{ shortcut = "2", name = "Zen" },
 	{ shortcut = "3", name = "Visual Studio Code" },
 	{ shortcut = "4", name = "Logseq" },
 	{ shortcut = "5", name = "Zotero" },
 	{ shortcut = "6", name = "Slack" },
 	{ shortcut = "7", name = "Spotify" },
+	{ shortcut = "8", name = "Arc" },
 }
 
 -- Bind application shortcuts
